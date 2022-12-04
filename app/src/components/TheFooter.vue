47370d3bf2867547262bfe1f374d3139868bbb2c
ilpr <ilppor.r@hotmail.com>
2022-12-04 22:12:01 +1100

    add favicon

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="flex flex-col items-center w-full py-10 lg:py-16 text-lg">
-        <p class="font-semibold text-white">
+    <div class="flex flex-col items-center w-full py-10 lg:py-16">
+        <p class="font-semibold text-lg lg:text-xl text-white">
             Created by ilpr
         </p>
-        <div>
-            <a
+        <div class="mt-5">
+          <a
             href="https://github.com/ilpr"
-            class="fa-brands fa-github mt-2 text-2xl text-cyan cursor-pointer"
+            class="fa-brands fa-github text-2xl text-cyan cursor-pointer"
             target="_blank"
-            ></a>
+          ></a>
+          <a
+            href="https://twitter.com/ilprdev"
+            class="fa-brands fa-twitter ml-5 text-2xl text-cyan cursor-pointer"
+            target="_blank"
+          ></a>
         </div>
+          <a
+            href="https://github.com/ilpr/solmint"
+            class="mt-3 lg:text-lg text-pink hover:text-cyan cursor-pointer"
+            target="_blank"
+          >
+            Review the code
+          </a>
     </div>
 </template>
 
