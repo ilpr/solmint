3cf3c5701ed20f43130b0c11d81aef39e9615c10
ilpr <100009916+ilpr@users.noreply.github.com>
2022-12-17 19:53:06 +1100

    Update TheFooter.vue

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="flex flex-col items-center space-y-4 w-full py-10 text-sm lg:text-base">
 
         <!-- 'Created by' text. -->
@@ -12,21 +11,10 @@
           <a
             href="https://github.com/ilpr"
             class="fa-brands fa-github text-xl text-cyan cursor-pointer"
-=======
-    <div class="flex flex-col items-center w-full py-10 lg:py-16">
-        <p class="font-semibold text-lg lg:text-xl text-white">
-            Created by ilpr
-        </p>
-        <div class="mt-5">
-          <a
-            href="https://github.com/ilpr"
-            class="fa-brands fa-github text-2xl text-cyan cursor-pointer"
->>>>>>> f48cdf115ea00cbdf7108a48c8c5f0c9ec9291b2
             target="_blank"
           ></a>
           <a
             href="https://twitter.com/ilprdev"
-<<<<<<< HEAD
             class="fa-brands fa-twitter ml-5 text-xl text-cyan cursor-pointer"
             target="_blank"
           ></a>
@@ -41,19 +29,6 @@
           Review the code
         </a>
 
-=======
-            class="fa-brands fa-twitter ml-5 text-2xl text-cyan cursor-pointer"
-            target="_blank"
-          ></a>
-        </div>
-          <a
-            href="https://github.com/ilpr/solmint"
-            class="mt-3 lg:text-lg text-pink hover:text-cyan cursor-pointer"
-            target="_blank"
-          >
-            Review the code
-          </a>
->>>>>>> f48cdf115ea00cbdf7108a48c8c5f0c9ec9291b2
     </div>
 </template>
 
